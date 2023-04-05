3a91bbd2556951906563a6c28c4565ff71200111
piyushsarkale23 <piyushsarkale35@gmail.com>
2023-04-05 18:15:07 +0530

    Add project files.

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle c = new Circle(5);
+            c.CalcuateArea();
+            Console.WriteLine(c);
+
+            Rectangle r = new Rectangle(45,5);
+            r.CalcuateArea();
+            Console.WriteLine(r);
+        }
+    }
+}
---
+++ ConsoleApp8/Shape.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+   abstract public class Shape
+    {
+        public abstract void CalcuateArea();
+       
+    }
+    public class Circle : Shape
+    {
+        private int radius;
+        private double area;
+        public Circle(int radius)
+        {
+            this.radius = radius;
+        }
+        public override void CalcuateArea()
+        {
+            area = 3.14 * radius * radius;
+        }
+        public override string ToString()
+        {
+            return $"Area of Circle is {area}";
+        }
+    }
+        public class Rectangle : Shape
+        {
+            private int length;
+            private int width;
+            private int area;
+
+        public Rectangle(int length,int width)
+        {
+            this.length = length;   
+            this.width = width;
+        }
+            public override void CalcuateArea()
+            {
+                area = length * width;
+            }
+            public override string ToString()
+            {
+                return $"Area of rectangle is {area}";
+            }
+        }
+    
+
+}
